dad6db6b751d05521e5ea6edb82d50c53a5a6fdf
wajidhussain001 <147056326+wajidhussain001@users.noreply.github.com>
2023-11-03 16:07:08 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,78 @@
+# elif- else if
+time = 17
+if time < 12:
+ print("good morning")
+elif time < 16:
+ print("good afternoon")
+elif time < 18:
+ print("good evening")
+elif time < 24:
+ print("good night")
+else:
+ print("error")
+
+#for loops
+String = "Wajid"
+for temp in String:
+ print(temp)
+
+#for loop
+for temp1 in range (10):
+ print(temp1)
+for temp2 in range (5,11):
+ print(temp2)
+for temp3 in range (0,31,3):
+ print(temp3)
+List = ["bike","car","trucks","trains"]
+String1 = "Brilliant"
+for temp4 in List:
+ print(temp4)
+for temp5 in String1:
+ print(temp5)
+
+#While loop
+number = 10
+while number < 10:
+ print(number)
+number = number+1
+
+#functions
+
+def wajid():
+ print("hello wajid")
+def syed():
+ print("hello syed")
+def hussain():
+ print("hello hussain")
+wajid()
+syed()
+hussain()
+
+def addition(number1,number2):
+ print("hello addition")
+ print(number1+number2)
+addition(5,5)
+
+def addition(number1,number2):
+ print("hello addition")
+ print(number1)
+addition(5,5)
+
+def subtraction(number3,number4):
+ print("hello subtraction")
+ print(number4-number3)
+subtraction(5,5)
+
+def subtraction(number5,number6):
+ if number5>number6:
+   print("hurray")
+ else:
+   print("hahaha")
+subtraction(3,2)
+
+def subtraction(number7,number8):
+ if number7>number8:
+   print("hurray")
+ else:
+   print("hahaha")
+subtraction(2,3)
